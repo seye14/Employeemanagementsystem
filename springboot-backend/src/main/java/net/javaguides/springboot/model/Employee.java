bb1c2808513974f63a46fda22659dc8c236fc810
seye14 <64876512+seye14@users.noreply.github.com>
2022-10-23 00:14:15 -0500

    Add files via upload

---
+++ springboot-backend/src/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,64 @@
+package net.javaguides.springboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name")
+	private String firstname;
+	
+	@Column(name = "last_name")
+	private String lastname;
+	
+	@Column(name = "ssn")
+	private long SSN;
+	
+	public Employee() {
+		
+	}
+	
+	
+	public Employee(String firstname, String lastname, long sSN) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		SSN = sSN;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public long getSSN() {
+		return SSN;
+	}
+	public void setSSN(long sSN) {
+		SSN = sSN;
+	}
+	
+
+}
